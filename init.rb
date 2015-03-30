e18da1e9513ae27387b2f4f4c87dda9712cf7a86
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2015-03-31 01:08:02 +0300

    Fixed menus & permissions.

--- init.rb
+++ init.rb
@@ -16,17 +16,17 @@ Redmine::Plugin.register :release_logs do
   permission :manage_release_log_configurations, :release_log_configurations => [:index, :new, :create, :edit, :update, :destroy]
   permission :manage_release_log_queues, :release_log_queues => [:index, :new, :create, :edit, :update, :destroy]
 
-  permission :view_global_release_logs,
-             :release_logs_home => [:index, :search],
-             :release_logs => [:index, :show]
+  permission :view_global_release_logs, :release_logs_home => [:index, :search]
 
-  menu :top_menu, :release_logs, { :controller => 'release_logs_home', :action => 'index' }, :caption => 'Release logs', :if => lambda { |project| User.current.allowed_to_globally?(:view_global_release_logs) }
+  menu :top_menu, :release_logs, { :controller => 'release_logs_home', :action => 'index' }, :caption => 'Release logs', :if => -> (project) { User.current.allowed_to_globally?(:view_global_release_logs, {}) }
 
   menu :admin_menu, :release_log_configurations, { :controller => 'release_log_configurations', :action => :index }, :caption => 'Release log configurations', :html => { :class => 'release-logs' }
   menu :admin_menu, :release_log_queues, { :controller => 'release_log_queues', :action => 'index' }, :caption => 'Release log queues', :html => { :class => 'release-log-queues' }, :after => :release_logs
 
   project_module :release_logs do
-    permission :view_project_release_logs, :release_logs => [:index, :show]
+    permission :view_project_release_logs,
+               :release_logs => [:index, :show]
+
     permission :manage_project_release_logs,
                :release_logs => [:new, :create, :edit, :update, :destroy, :clone, :send_notification],
                :release_log_previews => [:release_log,

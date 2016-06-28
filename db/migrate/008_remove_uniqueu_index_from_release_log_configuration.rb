d6afa7dd82be912e27b4c657f4144dec5a274d28
Daniel Vijge <danielvijge@users.noreply.github.com>
2016-06-28 11:30:27 +0200

    Allow proper uninstall

--- db/migrate/008_remove_uniqueu_index_from_release_log_configuration.rb
+++ db/migrate/008_remove_uniqueu_index_from_release_log_configuration.rb
@@ -1,5 +1,5 @@
 class RemoveUniqueuIndexFromReleaseLogConfiguration < ActiveRecord::Migration
   def change
-    remove_index :release_log_configurations, :name => :rl_conf_project_id
+    remove_index :release_log_configurations, :column => :project_id, :name => :rl_conf_project_id
   end
 end
